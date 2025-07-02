c2989ba39cb51ff5ceac5d60442bba7c5f192f73
RyannKim327 <rksesgundo123@gmail.com>
2025-07-02 19:01:44 +0800

    07-02-25 19:01 - @RyannKim327

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from g4f import Client, Provider
 from utils.gist import fetch_gist, update_gist
 
 app = Flask(__name__, static_url_path="/static")
-text_model = "gpt-4o-mini" # "llama-3.3-70b"
+text_model = "gpt-4o-mini"  # "llama-3.3-70b"
 image_model = "flux"
 
 CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -17,172 +17,169 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/")
 def main():
-  return app1()
+    return app1()
 
 
 @app.route("/api")
 def app1():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.errorhandler(404)
 def error(err):
-  return {
-    "status": 404,
-    "response": "Page not found, kindly check to our documentation",
-    "redirect": request.host,
-  }
+    return {
+        "status": 404,
+        "response": "Page not found, kindly check to our documentation",
+        "redirect": request.host,
+    }
 
 
 def generate_image(prompt):
-  client = Client()
-  response = client.images.generate(
-    # provider=Provider.Copilot,
-    model=image_model,
-    prompt=prompt,
-    response_format="url",
-  )
-  return response.data[0].url
+    client = Client()
+    response = client.images.generate(
+        # provider=Provider.Copilot,
+        model=image_model,
+        prompt=prompt,
+        response_format="url",
+    )
+    return response.data[0].url
 
 
 def checkImager(prompt):
-  client = Client()
-  format = json.dumps({"img": "boolean", "message": "string"})
-  msg = [
-    {
-      "role": "user",
-      "content": f"""Check weather last chat or prompt from user is asking for image generator, if the user ask you to generate image please generate, if not then dont generate and return to "img" key is False from the formet I gave to you.
+    client = Client()
+    format = json.dumps({"img": "boolean", "message": "string"})
+    msg = [
+        {
+            "role": "user",
+            "content": f"""Check weather last chat or prompt from user is asking for image generator, if the user ask you to generate image please generate, if not then dont generate and return to "img" key is False from the formet I gave to you.
       Your response must be in single line and with this format: {format}.
-      If is that an image, imagine that you\'ve generate the image, create a prompt for an image generator that nearly related to the prompt, or enhance the prompt given to make it more understandable by the AI.""",
-    }
-  ]
-  #   {
-  #       "role": "user",
-  #       "content": f'I have this prompt: "{prompt}". I want you to identify it based on the format I will given. The format was {json.dumps(format)}. If is that an image, imagine that you\'ve generate the image, create a prompt for an image generator that nearly related to the prompt, or enhance the prompt given to make it more understandable by the AI. The response, please `make it one line and remove the unwanted characters to prevent json parse error',
-  #   }
-  response = client.chat.completions.create(
-    model=text_model,
-    messages=msg + prompt
-  )
-  try:
-    # print(response.choices[0].message.content)
-    res = json.loads(response.choices[0].message.content)
-    # print(res)
-    if res["img"]:
-      generated_image = generate_image(res["message"])
-      return {
-        "status": 200,
-        "response": f"{res['message']}<br><br>![generated image]({generated_image})",
-        "image": generated_image,
-      }
-  except JSONDecodeError as e:
-    return checkImager(prompt)
+      If is that an image, imagine that you\'ve generate the image, create a prompt, and explain and identify what about to request for an image generator that nearly related to the prompt, or enhance the prompt given to make it more understandable by the AI.""",
+        }
+    ]
+    #   {
+    #       "role": "user",
+    #       "content": f'I have this prompt: "{prompt}". I want you to identify it based on the format I will given. The format was {json.dumps(format)}. If is that an image, imagine that you\'ve generate the image, create a prompt for an image generator that nearly related to the prompt, or enhance the prompt given to make it more understandable by the AI. The response, please `make it one line and remove the unwanted characters to prevent json parse error',
+    #   }
+    response = client.chat.completions.create(model=text_model, messages=msg + prompt)
+    try:
+        # print(response.choices[0].message.content)
+        res = json.loads(response.choices[0].message.content)
+        # print(res)
+        if res["img"]:
+            generated_image = generate_image(res["message"])
+            return {
+                "status": 200,
+                "response": f"{res['message']}<br><br>![generated image]({generated_image})",
+                "image": generated_image,
+            }
+    except JSONDecodeError as e:
+        return checkImager(prompt)
 
 
 @app.route("/api/chat/", methods=["POST", "GET"])
 def api_chat():
-  client = Client()
-
-  if request.method == "POST":
-    req = request.get_json()
-    websearch = True
-    if req and "websearch" in req:
-      websearch = req["websearch"]
-
-    img = checkImager(req["messages"])
-
-    if img:
-      return img
-
     client = Client()
-    response = client.chat.completions.create(
-      model=text_model,
-      # provider=Provider.Together,
-      web_search=websearch,
-      messages=req["messages"],
-    )
-    return {"status": 200, "response": response.choices[0].message.content}
-  else:
-    base = {}
-    req = request.args
-    msgs = [{"role": "user", "content": str(req.get("message"))}]
 
-    if req and "message" not in req:
-      return {"status": 404, "response": "Undefined message query"}
-    if req and "u" in req:
-      gist = fetch_gist()
-      base = gist
-      if req.get("message") == "clear" or req.get("message") == "cls":
-        gist["prompts"][req.get("u")] = []
-        update_gist(gist)
-        return {"status": 200, "response": "Queries are now cleared"}
-      else:  # if req.get("u") in gist["users"]:
-        try:
-          if gist["prompts"][req.get("u")]:
-            msgs = gist["prompts"][req.get("u")]
-            msgs.append({"role": "user", "content": req.get("message")})
-        except:
-          gist["prompts"][req.get("u")] = [
-            {"role": "user", "content": req.get("message")}
-          ]
+    if request.method == "POST":
+        req = request.get_json()
+        websearch = True
+        if req and "websearch" in req:
+            websearch = req["websearch"]
 
-    websearch = True
+        img = checkImager(req["messages"])
 
-    if req and "websearch" in req:
-      websearch = req.get("websearch")
+        if img:
+            return img
 
-    img = checkImager(msgs)
+        client = Client()
+        response = client.chat.completions.create(
+            model=text_model,
+            # provider=Provider.Together,
+            web_search=websearch,
+            messages=req["messages"],
+        )
+        return {"status": 200, "response": response.choices[0].message.content}
+    else:
+        base = {}
+        req = request.args
+        msgs = [{"role": "user", "content": str(req.get("message"))}]
 
-    if img:
-      return img
+        if req and "message" not in req:
+            return {"status": 404, "response": "Undefined message query"}
+        if req and "u" in req:
+            gist = fetch_gist()
+            base = gist
+            if req.get("message") == "clear" or req.get("message") == "cls":
+                gist["prompts"][req.get("u")] = []
+                update_gist(gist)
+                return {"status": 200, "response": "Queries are now cleared"}
+            else:  # if req.get("u") in gist["users"]:
+                try:
+                    if gist["prompts"][req.get("u")]:
+                        msgs = gist["prompts"][req.get("u")]
+                        msgs.append({"role": "user", "content": req.get("message")})
+                except:
+                    gist["prompts"][req.get("u")] = [
+                        {"role": "user", "content": req.get("message")}
+                    ]
 
-    response = client.chat.completions.create(
-      # provider=Provider.Together,
-      model=text_model,
-      messages=msgs,
-      web_search=websearch,
-    )
+        websearch = True
 
-    if req and "u" in req:
-      msgs.append(
-        {
-          "role": "system",
-          "content": response.choices[0].message.content,
-        }
-      )
-      base["prompts"][req.get("u")] = msgs
-      # gist.get("prompt").get(req.get("u")) = msgs
-      update_gist(base)
-    return {"status": 200, "response": response.choices[0].message.content}, 200
+        if req and "websearch" in req:
+            websearch = req.get("websearch")
+
+        img = checkImager(msgs)
+
+        if img:
+            return img
+
+        response = client.chat.completions.create(
+            # provider=Provider.Together,
+            model=text_model,
+            messages=msgs,
+            web_search=websearch,
+        )
+
+        if req and "u" in req:
+            msgs.append(
+                {
+                    "role": "system",
+                    "content": response.choices[0].message.content,
+                }
+            )
+            base["prompts"][req.get("u")] = msgs
+            # gist.get("prompt").get(req.get("u")) = msgs
+            update_gist(base)
+        return {"status": 200, "response": response.choices[0].message.content}, 200
 
 
 @app.route("/api/generate/", methods=["GET"])
 def api_generate():
-  return {"status": 200, "responses": generate_image(request.args.get("message"))}
+    return {"status": 200, "responses": generate_image(request.args.get("message"))}
 
 
 @app.route("/api/register/<string:id>/", methods=["POST", "GET"])
 def register(id):
-  gist = fetch_gist()
-  if "error" in gist.keys():
-    return
+    gist = fetch_gist()
+    if "error" in gist.keys():
+        return
 
-  req = request.args  # request.get_json()
+    req = request.args  # request.get_json()
 
-  if gist["prompts"].get(id) == None:
-    gist["prompts"][id] = []
-    if req and "roleplay" in req:
-      gist["prompts"][id] = [
-        {"role": "user", "content": req.get("roleplay")},
-        {"role": "system", "content": "Okay"},
-      ]
-    update_gist(gist)
-    return {"status": 200, "response": f"{id} User ID is now registered"}
-  else:
-    return {"status": 200, "response": "This user is already existed"}
+    if gist["prompts"].get(id) == None:
+        gist["prompts"][id] = []
+        if req and "roleplay" in req:
+            gist["prompts"][id] = [
+                {"role": "user", "content": req.get("roleplay")},
+                {"role": "system", "content": "Okay"},
+            ]
+        update_gist(gist)
+        return {"status": 200, "response": f"{id} User ID is now registered"}
+    else:
+        return {"status": 200, "response": "This user is already existed"}
 
 
 if __name__ == "__main__":
-  app.run("0.0.0.0", 7000)
+    app.run("0.0.0.0", 7000)
 
 # NOTE: To test, execute flask --app main.py --debug run
