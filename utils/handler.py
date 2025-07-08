706cb3b9ae554ad37335a317e52efa48674a7d14
RyannKim327 <rksesgundo123@gmail.com>
2025-07-08 14:12:00 +0800

    07-08-25 14:101

--- utils/handler.py
+++ utils/handler.py
@@ -1,8 +1,8 @@
 from core.text import chat_handler
 
 def get_chat_handler(prompt: dict, websearch=True):
-
+    return chat_handler([prompt])
     pass
 
-def post_chat_handler(prompt: [], websearch=True):
+def post_chat_handler(prompt, websearch=True):
     return chat_handler(prompt, websearch)
