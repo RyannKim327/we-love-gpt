222e141eeced155eb79110c350a472c7fa7f246f
RyannKim327 <rksesgundo123@gmail.com>
2025-08-24 18:58:31 +0800

    08-24-25 18:58

--- utils/setup.py
+++ utils/setup.py
@@ -1,8 +1,9 @@
-""" INFO:
-  Author: @RyannKim327
-  Date Modified: 07-08-2025
-  Purpose: A setup and configurations
+"""INFO:
+Author: @RyannKim327
+Date Modified: 07-08-2025
+Purpose: A setup and configurations
 """
 
-text_model = "gpt-4o-mini"  # "llama-3.3-70b"
+text_model = "llama-3.3-70b"
 image_model = "flux"
+
